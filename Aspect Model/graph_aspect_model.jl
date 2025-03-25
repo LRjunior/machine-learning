44f4248d04b7a2268896be999f13bcf98a2b5643
Ladislav Rado <lr@rsw.sk>
2025-03-25 20:16:39 +0100

    Add files via upload

--- Aspect Model/graph_aspect_model.jl
+++ Aspect Model/graph_aspect_model.jl
@@ -5,9 +5,10 @@ begin
 	Pzd = readdlm("Pzd.txt", '\t', Float64, '\n')
 	Pwz = readdlm("Pwz.txt", '\t', Float64, '\n')
 	Pdz = readdlm("Pdz.txt", '\t', Float64, '\n')
+	Pzw = readdlm("Pzw.txt", '\t', Float64, '\n')
 	Z = size(Pzd, 1)
 
-	plt.subplot(1,3,1)
+	plt.subplot(1,4,1)
 	plt.title("P(z|d)")
 	plt.xlabel("d")
 	plt.ylabel("z")
@@ -16,7 +17,7 @@ begin
 	plt.colorbar()
 	plt.gcf()
 
-	plt.subplot(1,3,2)
+	plt.subplot(1,4,2)
 	plt.title("P(w|z)")
 	plt.xlabel("z")
 	plt.ylabel("w")
@@ -25,12 +26,21 @@ begin
 	plt.colorbar()
 	plt.gcf()
 
-	plt.subplot(1,3,3)
+	plt.subplot(1,4,3)
 	plt.title("P(d|z)")
 	plt.xlabel("z")
 	plt.ylabel("d")
 	plt.xticks(0.5:Z-0.5, 1:Z)
 	plt.pcolor(Pdz, cmap=plt.matplotlib.cm.binary, vmin=0, vmax=1)
+	plt.colorbar()
+	plt.gcf()
+	
+	plt.subplot(1,4,4)
+	plt.title("P(z|w)")
+	plt.xlabel("w")
+	plt.ylabel("z")
+	plt.yticks(0.5:Z-0.5, 1:Z)
+	plt.pcolor(Pzw, cmap=plt.matplotlib.cm.binary, vmin=0, vmax=1)
 	plt.colorbar()
 	plt.gcf()
 
